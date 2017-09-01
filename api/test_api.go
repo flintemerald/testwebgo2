2ca59b95936d72fe1cec5be1fa04b75b2f861f17
bc6a442277fb <you@example.com>
2017-09-01 13:04:25 +0300

    -

--- api/test_api.go
+++ api/test_api.go
@@ -7,7 +7,8 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"strconv"
-	
+	"crypto/sha1"
+  "encoding/hex"
 	
 	"../utils"
 )
@@ -24,12 +25,16 @@ func indexHandler() http.Handler {
 
 func indexTest1() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    resDict := make(map[string]interface{})
 		body, _ := ioutil.ReadAll(r.Body)
 		dict := make(map[string]string)
 		json.Unmarshal(body, &dict)
 		weight, _ := strconv.ParseInt(dict["weight"], 10, 32)
 		//time.Sleep(500 * time.Millisecond)
 		randString := utils.RandStringRunes(int(weight))
-		fmt.Fprintf(w, randString)
+    sum := sha1.Sum([]byte(randString))
+    resDict["answer"] = hex.EncodeToString(sum[:])
+    resString, _ := json.Marshal(resDict)
+		fmt.Fprintf(w, string(resString))
 	})
 }
