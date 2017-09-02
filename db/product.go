7bdf242b530623efcec6dcba47d541e5701f681a
bc6a442277fb <you@example.com>
2017-09-02 19:47:08 +0300

    ProductCompact and ProductFull

--- db/product.go
+++ db/product.go
@@ -4,15 +4,17 @@ import (
   "../models"
 )
 
-func GetProductsList() []models.Product {
-  res := []models.Product{}
+
+
+func GetProductsList() []models.ProductCompact {
+  res := []models.ProductCompact{}
   rows, err := con.Queryx("SELECT id, name_rus, name_eng FROM product LIMIT 9000")
   if err != nil {
     return res
   }
   for rows.Next() {
-    prod := models.Product{}
-    prod.Scan(rows)
+    prod := models.ProductCompact{}
+    rows.StructScan(&prod)
     res = append(res, prod)
   }
   return res
--- models/product.go
+++ models/product.go
@@ -1,15 +1,21 @@
 package models
 
 import (
-  "github.com/jmoiron/sqlx"
 )
 
 
 
-type Product struct {
+type ProductCompact struct {
   Id int64
   Name_rus string
   Name_eng string
+}
+
+
+
+type ProductFull struct {
+  ProductCompact
+  
   Photo_url string
   Year string
   Vendor_code string
@@ -19,8 +25,4 @@ type Product struct {
   Description string
   Grape_sort string
   Price int64
-}
-
-func (o *Product) Scan(rows *sqlx.Rows) {
-  rows.StructScan(&o)
 }
